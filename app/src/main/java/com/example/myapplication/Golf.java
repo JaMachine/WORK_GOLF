a7c3b0eafd6b3d37e819d0354d84b7b865b3b66f
yuriy <yuriy@mail.com>
2022-02-02 14:00:59 +0200

    theme

--- app/src/main/java/com/example/myapplication/Golf.java
+++ app/src/main/java/com/example/myapplication/Golf.java
@@ -15,11 +15,19 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityGolfBinding;
 
-public class Golf extends AppCompatActivity {
+public class Golf extends AppCompatActivity implements Theme{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_golf);
+        hideNavigationBar();
+    }
+
+    @Override
+    public void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Theme.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+public interface Theme {
+    void hideNavigationBar();
+}
